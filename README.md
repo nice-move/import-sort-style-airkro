ab31956227e11792c8b6be1dff721fa75524b0da
Eric Chen <airkro@qq.com>
2017-11-23 15:56:09 +0800

    v0.0.5

---
+++ README.md
@@ -0,0 +1,26 @@
+# Import Sort Style Antd
+
+## Installation
+
+```cmd
+npm install -O github:airkro/import-sort-style-antd
+```
+
+## Usage
+
+Add following settings to your `package.json`:
+
+```json
+{
+  "importSort": {
+    ".js, .jsx": {
+      "parser": "babylon",
+      "style": "antd"
+    }
+  }
+}
+```
+
+## Related
+
+<https://github.com/renke/import-sort>
---
+++ src/index.ts
@@ -0,0 +1,154 @@
+import { IComparatorFunction, IMatcherFunction, IStyleAPI, IStyleItem } from 'import-sort-style';
+
+const setSeparator = { separator: true };
+
+const isStylesModule: IMatcherFunction = imported =>
+  Boolean(imported.moduleName.match(/\.(s?css|less|postcss)$/));
+
+const isImagesModule: IMatcherFunction = imported =>
+  Boolean(imported.moduleName.match(/\.(jpe?g|png|svg)$/));
+
+const isSiblingModule: IMatcherFunction = imported => Boolean(imported.moduleName.match(/^\.\//));
+
+const isModule = (name: string) => (moduleName: string) => moduleName === name;
+
+const oneOfModule = (modules: string[]) => (moduleName: string) => modules.indexOf(moduleName) > -1;
+
+const aboutModule = (name: string) => (moduleName: string) => moduleName.indexOf(name) > -1;
+
+export default (styleApi: IStyleAPI): IStyleItem[] => {
+  const {
+    alias,
+    and,
+    dotSegmentCount,
+    hasNoMember,
+    hasOnlyDefaultMember,
+    isAbsoluteModule,
+    isNodeModule,
+    isRelativeModule,
+    member,
+    moduleName,
+    naturally,
+    not,
+    startsWithLowerCase,
+    startsWithUpperCase,
+    unicode
+  } = styleApi;
+
+  const sortNamedMembers = alias(unicode);
+  return [
+    {
+      // import "./foo"
+      match: and(hasNoMember, isRelativeModule, not(isStylesModule)),
+      sort: [dotSegmentCount]
+    },
+    setSeparator,
+    {
+      // import "foo"
+      match: and(hasNoMember, isAbsoluteModule, not(moduleName(aboutModule('moment'))))
+    },
+    setSeparator,
+    {
+      // import … from "fs";
+      // import … from "path";
+      sortNamedMembers,
+      match: isNodeModule,
+      sort: moduleName(naturally)
+    },
+    setSeparator,
+    {
+      // import Webpack from "webpack"
+      sortNamedMembers,
+      match: moduleName(isModule('webpack'))
+    },
+    {
+      sortNamedMembers,
+      match: moduleName(aboutModule('webpack'))
+    },
+    setSeparator,
+    {
+      // import React from "react"
+      sortNamedMembers,
+      match: moduleName(isModule('react'))
+    },
+    {
+      sortNamedMembers,
+      match: moduleName(isModule('prop-types'))
+    },
+    {
+      sortNamedMembers,
+      match: moduleName(isModule('react-dom'))
+    },
+    {
+      sortNamedMembers,
+      match: moduleName(isModule('react-router-dom'))
+    },
+    {
+      sortNamedMembers,
+      match: moduleName(isModule('redux'))
+    },
+    {
+      sortNamedMembers,
+      match: moduleName(aboutModule('redux'))
+    },
+    {
+      sortNamedMembers,
+      match: moduleName(isModule('antd'))
+    },
+    {
+      sortNamedMembers,
+      match: moduleName(
+        oneOfModule(['qs', 'axios', 'classnames', 'immutable', 'lodash', 'md5', 'flat', 'is-url'])
+      )
+    },
+    {
+      sortNamedMembers,
+      match: moduleName(isModule('moment'))
+    },
+    {
+      sortNamedMembers,
+      match: moduleName(aboutModule('moment'))
+    },
+    {
+      sortNamedMembers,
+      match: moduleName(aboutModule('react'))
+    },
+    {
+      // import foo from "bar"
+      sortNamedMembers,
+      match: isAbsoluteModule,
+      sort: moduleName(naturally)
+    },
+    setSeparator,
+    {
+      // import … from "./foo";
+      // import … from "../foo";
+      sortNamedMembers,
+      match: and(isRelativeModule, isSiblingModule, not(isImagesModule), not(isStylesModule)),
+      sort: [dotSegmentCount, moduleName(naturally)]
+    },
+    setSeparator,
+    {
+      // import … from "./foo";
+      // import … from "../foo";
+      sortNamedMembers,
+      match: and(isRelativeModule, not(isSiblingModule), not(isImagesModule), not(isStylesModule)),
+      sort: [dotSegmentCount, moduleName(naturally)]
+    },
+    setSeparator,
+    {
+      // import "./style.css";
+      sortNamedMembers,
+      match: isStylesModule,
+      sort: [dotSegmentCount]
+    },
+    setSeparator,
+    {
+      // import "./image.jpg";
+      sortNamedMembers,
+      match: isImagesModule,
+      sort: [dotSegmentCount, moduleName(naturally)]
+    },
+    setSeparator
+  ];
+};
